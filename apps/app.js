937b09734375727acccce01f7fa1806797d193ed
Caleb Sherfick <calebsherfick@gmail.com>
2019-02-06 14:22:48 -0700

    program works. commented out buttons to start styling

--- apps/app.js
+++ apps/app.js
@@ -1,20 +1,62 @@
 console.log('Start')
 
-function play(playerChoice, computerChoice) {
-  // let computerChoice = rock
+
+
+
+
+// function computerChoice() {
+//   // Math.floor(Math.random() * );
+// }
+
+let comOptions = {
+  rock: 'rock',
+  paper: 'paper',
+  scissors: 'scissors'
+}
+
+
+function setComputerChoice() {
+  let choices = Object.keys(comOptions)
+  let choiceIndex = Math.floor(Math.random() * choices.length)
+  let choice = choices[choiceIndex]
+  return choice
+}
+
+
+
+function play(playerChoice) {
+  let computerChoice = setComputerChoice()
+  //resultStr variable = ''
   if (playerChoice == 'rock') {
     if (computerChoice == 'rock') {
-      return "Woah, you TIED!"
+      window.alert("Woah, you TIED!")
     } else if (computerChoice == 'paper') {
-      return "You LOST, it's a wrap!"
+      window.alert("You LOST, it's a wrap!")
     } else {
-      return "You WON, you crushed em'!"
+      window.alert("You WON, you crushed em'!")
+    }
+  } else if (playerChoice == 'paper') {
+    if (computerChoice == 'rock') {
+      window.alert("You WON, they got smothered!")
+    } else if (computerChoice == 'paper') {
+      window.alert("Woah, you TIED!")
+    } else {
+      window.alert("You LOST, cut it out!")
+    }
+  } else if (playerChoice == 'scissors') {
+    if (computerChoice == 'rock') {
+      window.alert("You LOST, they crushed you!")
+    } else if (computerChoice == 'paper') {
+      window.alert("You WON, you cut them to shreds!")
+    } else {
+      window.alert("Woah, you TIED!")
     }
   }
 
 }
 
-console.log(play("rock", "scissors"))
+
+// console.log(play(playerChoice, computerChoice))
 
 
 
