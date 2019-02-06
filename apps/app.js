b4d8511fb0bd9707414e5d4d99d9b7679830ba4d
Caleb Sherfick <calebsherfick@gmail.com>
2019-02-06 13:34:22 -0700

    basic js structure for 'rock' complete

--- apps/app.js
+++ apps/app.js
@@ -1,7 +1,23 @@
-function play(playerChoice) {
-  let computerChoise = rock
+console.log('Start')
 
+function play(playerChoice, computerChoice) {
+  // let computerChoice = rock
+  if (playerChoice == 'rock') {
+    if (computerChoice == 'rock') {
+      return "Woah, you TIED!"
+    } else if (computerChoice == 'paper') {
+      return "You LOST, it's a wrap!"
+    } else {
+      return "You WON, you crushed em'!"
+    }
+  }
 
-
-  return (playerChoice)
 }
+
+console.log(play("rock", "scissors"))
+
+
+
+
+
+console.log('Finish')
