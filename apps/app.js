357b9a1bd38b80420649fc4b2a977246666ebdc1
Caleb Sherfick <calebsherfick@gmail.com>
2019-02-06 22:39:46 -0700

    added a hit counter

--- apps/app.js
+++ apps/app.js
@@ -7,6 +7,10 @@ console.log('Start')
 // function computerChoice() {
 //   // Math.floor(Math.random() * );
 // }
+let playerWins = 0;
+let compWins = 0;
+let tieCount = 0;
+
 
 let comOptions = {
   punch: 'punch',
@@ -32,38 +36,59 @@ function play(playerChoice) {
   if (playerChoice == 'punch') {
     if (computerChoice == 'punch') {
       resultStr = "You TIED: You fist bumped each other..."
+      tieCount++;
     } else if (computerChoice == 'slap') {
       resultStr = "You LOST: They caught your punch in their hand!"
+      compWins++;
     } else {
       resultStr = "You WON: You knocked them in the head!"
+      playerWins++;
     }
   } else if (playerChoice == 'slap') {
     if (computerChoice == 'punch') {
       resultStr = "You WON: You caught their punch in your hand!"
+      playerWins++;
     } else if (computerChoice == 'slap') {
       resultStr = "You TIED: You high fived each other..."
+      tieCount++;
     } else {
       resultStr = "You LOST: They poked you in the eyes!"
+      compWins++;
     }
   } else if (playerChoice == 'eyes') {
     if (computerChoice == 'punch') {
       resultStr = "You LOST: They punched you in the face!"
+      compWins++;
     } else if (computerChoice == 'slap') {
       resultStr = "You WON: You countered their slap with an eye poke!"
+      playerWins++;
     } else {
       resultStr = "You TIED: You poked each other in the eyes..."
+      tieCount++;
     }
   }
   computerResult()
+  scoreKeeper()
 }
 
 
 function computerResult() {
   document.getElementById('result').innerHTML = `
   <h1>Your opponent chose ${computerChoice}.</h1>
-  <h2>${resultStr}</h2>`
+  <h2>${resultStr}</h2>
+  <br>
+  <h1>Go ahead and make another move</h1>`
 }
-// computerResult()
+
+
+
+
+function scoreKeeper() {
+  document.getElementById('playerWins').innerText = `Your Strikes Hit: ${playerWins}`
+  document.getElementById('tieCount').innerText = `Ties: ${tieCount}`
+  document.getElementById('compWins').innerText = `Opponent Strikes Hit: ${compWins}`
+}
+
 
 
 
